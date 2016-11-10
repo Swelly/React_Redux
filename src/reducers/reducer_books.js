660710ab9c1ec800a7b0f08302dcc1ae4dbc7e00
Swelly <w.magness17@gmail.com>
2016-11-10 14:42:59 -0500

    container entry

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function () {
+	return [
+		{title: 'Javascript: The Good Parts'},
+		{title: 'Harry Potter: Sorcerer\'s stone'},
+		{title: 'The Hero with a Thousand Faces'},
+		{title: 'The Art of Princess Mononoke'},
+		{title: 'The Count of Monte Cristo'}
+	]
+}
